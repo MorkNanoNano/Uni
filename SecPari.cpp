ac429b2523c256396ea30c4d0826341514c25113
MorkNanoNano <98193256+MorkNanoNano@users.noreply.github.com>
2022-01-25 12:31:58 +0100

    Update SecPari.cpp

--- SecPari.cpp
+++ SecPari.cpp
@@ -6,28 +6,28 @@ bool sec_pari(int *x, int dimx, int p);
 
 int main(){
 
-    int dim = 10;
-    int x[dim] = {10,3,6,4,8,5,7,2,5,7};
-    int n = 3;
+    int dimX = 10;
+    int x[dimX] = {10,3,6,4,8,5,7,2,5,7};
+    int p = 3;
 
-    if(sec_pari(x,dim,n))
+    if(sec_pari(x,dimX,p))
         cout << "BOB" << endl;
 
     return 0;
 }
 
-bool sec_pari(int *x, int dimx, int p){
+bool sec_pari(int *x, int dimX, int p){
     bool condition = false;
     int count = 0;
-    for(int i = 0; i < dimx; i++){
+    for(int i = 0; i < dimX; i++){
         while(x[i] % 2 == 0){
             count ++;
         }
-    if(count == p)
-        condition = true;
+        if(count >= p)
+            condition = true;
 
-    else if(count != p)
-        count = 0;
+        else if(count < p)
+            count = 0;
     }
 return condition;
 }
